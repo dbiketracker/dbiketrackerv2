8899eac1e0f5f8989827b37ad9ce7765f83e418e
Aaron O'Hare <Aaron O'Hare>
2016-11-30 21:39:28 +0000

    index

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
         $db = $conn->heroku_2g7zhsrw;
         echo (" **Connection to database successful** ");
         echo($conn);
-        $collection = $db->stations;
-        echo " **Station database selected**  <br><br>";
+    //    $collection = $db->stations;
+  //      echo " **Station database selected**  <br><br>";
        
 
         $dbd = json_decode($json_array);
         print_r($dbd);
 //        $collection->insert($dbd);
-        $conn->close();
+//        $conn->close();
         ?>
     </head>
 </html>
