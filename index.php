ac861aeb28404a57f8ffef7b8ece90728de662ab
Aaron O'Hare <Aaron O'Hare>
2016-11-30 21:35:39 +0000

    index

--- index.php
+++ index.php
@@ -16,18 +16,19 @@
         
         
         
-//        $uri = "mongodb://heroku_23dw8240:m6fb3mnio6qeob18q8m96ttc1l@ds147487.mlab.com:47487/heroku_23dw8240";
-//        $conn = new Mongo($uri);
-//        $db = $conn->heroku_23dw8240;
-//        echo (" **Connection to database successful** ");
-//        echo($conn);
-//        $collection = $db->stations;
-//        echo " **Station database selected**  <br><br>";
-//
+        $uri = "mongodb://heroku_2g7zhsrw:fptu2g7faerobjk513p7frl9sq@ds013222.mlab.com:13222/heroku_2g7zhsrw";
+        $conn = new Mongo($uri);
+        $db = $conn->heroku_2g7zhsrw;
+        echo (" **Connection to database successful** ");
+        echo($conn);
+        $collection = $db->stations;
+        echo " **Station database selected**  <br><br>";
+       
+
         $dbd = json_decode($json_array);
         print_r($dbd);
 //        $collection->insert($dbd);
-//        $conn->close();
+        $conn->close();
         ?>
     </head>
 </html>
