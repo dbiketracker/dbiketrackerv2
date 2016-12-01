687f9705f69cfb25df910fa2cc798cd58984304b
Dike Tracker <d.biketracker@gmail.com>
2016-12-01 17:32:01 +0000

    view just few fields

--- index.php
+++ index.php
@@ -11,11 +11,18 @@
         //phpinfo();
         $url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
         $json_array = file_get_contents($url);
+        $json = json_decode($json_array, true);
+        foreach ($json['number']['name'] as $item) {
+            print $item['number'];
+            print " - ";
+            print $item['name'];
+        }
+        print_r($json);
         //$converted = json_decode($json_array,true);
         //print_r($json_array);
-        
-        
-        
+
+
+
         $uri = "mongodb://heroku_2g7zhsrw:fptu2g7faerobjk513p7frl9sq@ds013222.mlab.com:13222/heroku_2g7zhsrw";
         $conn = new Mongo($uri);
         $db = $conn->heroku_2g7zhsrw;
@@ -23,10 +30,11 @@
         echo($conn);
         $collection = $db->stationsv2;
         echo " **Station database selected**  <br><br>";
-       
 
-        $dbd = json_decode($json_array, true);
-        print_r ($dbd[0]->number->name[0]->value); 
+
+
+
+//        $dbd = json_decode($json_array, true);
 //        print_r($dbd);
 //        $collection->insert($dbd);
         $conn->close();
